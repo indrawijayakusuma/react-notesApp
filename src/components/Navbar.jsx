65b80a5b86bcf90f56332525ab7dd9c4e48d9425
indrawijayakusuma <indrawk56@gmail.com>
2023-10-20 17:45:59 +0700

    mengerjakan view arsip dan active notes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,8 @@ class Navbar extends React.Component {
         this.state = {
             search: '',
         }
+
+        this.onChangeEventHandler = this.onChangeEventHandler.bind(this);
     }
 
     onChangeEventHandler(event) {
@@ -14,17 +16,18 @@ class Navbar extends React.Component {
                 search: event.target.value,
             }
         })
+        console.log(this.state.search)
     }
 
     render() {
         return (
             <>
-                <div className="flex flex-row justify-between px-40 mx-auto h-16 items-center border-y-2">
-                    <h1 className="text-2xl font-bold">Notes</h1>
-                    <div>
-                        <form action="">
-                            <input className="border-2 border-gray-500 rounded-md h-9 w-56 px-2" type="text" placeholder="search"/>
-                        </form>
+                <div className="flex flex-row w-full px-28 mx-auto h-20 items-center border-y-2">
+                    <div className="w-[50%]">
+                        <h1 className="text-4xl font-bold">Notes</h1>
+                    </div>
+                    <div className="w-[50%] flex justify-end">
+                        <input className="border-2 border-gray-300 w-3/5 rounded-lg h-11 px-2" value={this.state.search} onChange={this.onChangeEventHandler} type="text" placeholder="search"/>
                     </div>
                 </div>
             </>
--- src/components/NotesApp.jsx
+++ src/components/NotesApp.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { getInitialData } from "../utils";
 import Navbar from './Navbar';
-// import FormNotes from './FormNotes';
-// import ActiveNotes from './ActiveNotes';
-// import ArsipNotes from './ArsipNotes';
+import FormNotes from './FormNotes';
+import NotesList from './NotesList';
 
 class NotesApp extends React.Component {
     constructor(props) {
@@ -11,16 +10,38 @@ class NotesApp extends React.Component {
         this.state = {
             notes: getInitialData(),
         }
+
+        this.onAddNotesHandler = this.onAddNotesHandler.bind(this);
     }
 
     OnSearchHandler() {
         
     }
 
+    onAddNotesHandler({ title, body }) {
+        this.setState((prevState) => {
+            return {
+                notes: [
+                    ...prevState.notes,
+                    {
+                        id: +new Date(),
+                        title,
+                        body,
+                        createdAt: +new Date(),
+                        archived: false,
+                    }
+                ]
+            }
+        })
+    }
+
     render() {
         return (
             <>
                 <Navbar/>
+                <FormNotes addNotes={this.onAddNotesHandler}/>
+                <NotesList notes={this.state.notes.filter(note => note.archived !== true)}/>
+                <NotesList notes={this.state.notes.filter(note => note.archived === true)}/>
             </>
             
         )
